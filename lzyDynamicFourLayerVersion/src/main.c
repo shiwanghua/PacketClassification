3bf1a3b28e6f585197e4ca795733fbbbbd582fec
shiwanghua <2804324275@qq.com>
2022-08-01 09:51:24 +0800

    trans format

--- lzyDynamicFourLayerVersion/src/main.c
+++ lzyDynamicFourLayerVersion/src/main.c
@@ -7,10 +7,11 @@
 #include <sys/time.h>
 #include <assert.h>
 
-void test() {
-	srand((int) time(0));
-	ACL_rules datasets = {0, 0, 0};
-	ACL_messages message_sets = {0, 0, 0};
+void test()
+{
+	srand((int)time(0));
+	ACL_rules datasets = { 0, 0, 0 };
+	ACL_messages message_sets = { 0, 0, 0 };
 	read_rules("classbench_256k_s/acl1_256k.txt", &datasets);
 	read_messages("classbench_256k_s/acl1_256k_trace.txt", &message_sets);
 	double iCycle, oneMatch = 0, memoryAccess = 0;
@@ -20,7 +21,8 @@ void test() {
 	rule r = datasets.list[rand() % datasets.size];
 	count = 0;
 	iCycle = GetCPUCycle();
-	for (int i = 0; i < num; i++) {
+	for (int i = 0; i < num; i++)
+	{
 
 //		iCycle = GetCPUCycle();
 //		rule r = datasets.list[rand() % datasets.size];
@@ -32,10 +34,10 @@ void test() {
 		unsigned int p_sip, p_dip, r_ip;
 		memcpy(&p_sip, p.source_ip, 4);
 		memcpy(&p_dip, p.destination_ip, 4);
-		m_bit = 32 - (unsigned int) r.source_mask;
+		m_bit = 32 - (unsigned int)r.source_mask;
 		memcpy(&r_ip, r.source_ip, 4);
 		if (m_bit != 32 && p_sip >> m_bit != r_ip >> m_bit)count++;  //if source ip not match, check next
-		m_bit = 32 - (unsigned int) r.destination_mask;  //comput the bit number need to move
+		m_bit = 32 - (unsigned int)r.destination_mask;  //comput the bit number need to move
 		memcpy(&r_ip, r.destination_ip, 4);
 		if (m_bit != 32 && p_dip >> m_bit != r_ip >> m_bit)count++;  //if destination ip not match, check next
 		if (p.source_port < r.source_port[0] || r.source_port[1] < p.source_port)
@@ -43,17 +45,17 @@ void test() {
 		if (p.destination_port < r.destination_port[0] ||
 			r.destination_port[1] < p.destination_port)
 			count++;  //if destination port not match, check next
-		if (r.protocol[0] != 0 && p.protocol != (unsigned int) r.protocol[1]) count++;
-		oneMatch += (double) GetCPUCycle() - iCycle;
+		if (r.protocol[0] != 0 && p.protocol != (unsigned int)r.protocol[1]) count++;
+		oneMatch += (double)GetCPUCycle() - iCycle;
 	}
 //	oneMatch+=(double) GetCPUCycle() - iCycle;
 	printf("count=%d, num=%d, oneMessageMatchOneRule= %f\n\nmemoryAccess=%f\n", count, num, oneMatch / num,
-		   memoryAccess / (2 * num));
+		memoryAccess / (2 * num));
 
 }
 
-
-int main() {
+int main()
+{
 	int core_id = 8;
 
 	cpu_set_t mask;
@@ -62,7 +64,7 @@ int main() {
 	sched_setaffinity(0, sizeof(mask), &mask);
 
 //	test();
-	srand((int) time(0));
+	srand((int)time(0));
 #if PROTO
 	printf("protocol %d\n", PROTO_LAYER);
 #endif
@@ -106,31 +108,57 @@ int main() {
 								 "acl4_256k_trace.txt", "acl5_256k_trace.txt", "fw1_256k_trace.txt",
 								 "fw2_256k_trace.txt", "fw3_256k_trace.txt", "fw4_256k_trace.txt", "fw5_256k_trace.txt",
 								 "ipc1_256k_trace.txt", "ipc2_256k_trace.txt"};
-	*/	
+	*/
+
+
+//	char ruleFileName[numDataSets][100] = {"", "rules2.txt", "rules3.txt", "rules4.txt", "rules5.txt", "rules6.txt"};
+//	char headFileName[numDataSets][100] = {"", "tuples2.txt", "tuples3.txt", "tuples4.txt", "tuples5.txt", "tuples6.txt"};
 
 	const int numDataSets = 17;
-	char ruleFileName[numDataSets][100] = {"", "rules2.txt", "rules3.txt", "rules4.txt", "rules5.txt", "rules6.txt"};
-	char headFileName[numDataSets][100] = {"", "tuples2.txt", "tuples3.txt", "tuples4.txt", "tuples5.txt", "tuples6.txt"};
-
+	char ruleFileName[numDataSets][50] = { "acl128_same_fmt_with_cb256k/acl_rules2_cb.txt",
+										   "acl128_same_fmt_with_cb256k/acl_rules3_cb.txt",
+										   "acl128_same_fmt_with_cb256k/acl_rules4_cb.txt",
+										   "acl128_same_fmt_with_cb256k/acl_rules5_cb.txt",
+										   "acl128_same_fmt_with_cb256k/acl_rules6_cb.txt",
+										   "classbench_256k/acl1_256k.txt", "classbench_256k/acl2_256k.txt",
+										   "classbench_256k/acl3_256k.txt", "classbench_256k/acl4_256k.txt",
+										   "classbench_256k/acl5_256k.txt", "classbench_256k/fw1_256k.txt",
+										   "classbench_256k/fw2_256k.txt", "classbench_256k/fw3_256k.txt",
+										   "classbench_256k/fw4_256k.txt", "classbench_256k/fw5_256k.txt",
+										   "classbench_256k/ipc1_256k.txt", "classbench_256k/ipc2_256k.txt"};
+	char headFileName[numDataSets][50] = { "acl128_same_fmt_with_cb256k/acl_tuples2_cb.txt",
+										   "acl128_same_fmt_with_cb256k/acl_tuples3_cb.txt",
+										   "acl128_same_fmt_with_cb256k/acl_tuples4_cb.txt",
+										   "acl128_same_fmt_with_cb256k/acl_tuples5_cb.txt",
+										   "acl128_same_fmt_with_cb256k/acl_tuples6_cb.txt",
+										   "classbench_256k/acl1_256k_trace.txt", "classbench_256k/acl2_256k_trace.txt",
+										   "classbench_256k/acl3_256k_trace.txt",
+										   "classbench_256k/acl4_256k_trace.txt", "classbench_256k/acl5_256k_trace.txt",
+										   "classbench_256k/fw1_256k_trace.txt",
+										   "classbench_256k/fw2_256k_trace.txt", "classbench_256k/fw3_256k_trace.txt",
+										   "classbench_256k/fw4_256k_trace.txt",
+										   "classbench_256k/fw5_256k_trace.txt", "classbench_256k/ipc1_256k_trace.txt",
+										   "classbench_256k/ipc2_256k_trace.txt"};
 	double totalMatchCycle = 0;
 	double totalInsertCycle = 0;
 
-	for (int q = 1; q < numDataSets; q++) {
-		ACL_rules datasets = {0, 0, 0};
-		ACL_messages message_sets = {0, 0, 0};
+	for (int q = 1; q < numDataSets; q++)
+	{
+		ACL_rules datasets = { 0, 0, 0 };
+		ACL_messages message_sets = { 0, 0, 0 };
 //		Cell *index;
 //		index = (Cell *) calloc(CELL_SIZE, sizeof(Cell));
 
-#if useShuffleRule == 0
-		char tmpFileName[100] = "classbench_256k_s/";
-#elif useShuffleRule == 1
-		char tmpFileName[100] = "classbench_256k/";
-#else
-		char tmpFileName[100] = "acl_128k_test/";
-#endif
-		strcat(tmpFileName, ruleFileName[q]);
+//#if useShuffleRule == 0
+//		char tmpFileName[100] = "classbench_256k_s/";
+//#elif useShuffleRule == 1
+//		char tmpFileName[100] = "classbench_256k/";
+//#else
+//		char tmpFileName[100] = "acl_128k_test/";
+//#endif
+//		strcat(tmpFileName, ruleFileName[q]);
 //		read_rules(tmpFileName, &datasets);
-		read_rules_bitset(tmpFileName, &datasets);
+		read_rules_bitset(ruleFileName[q], &datasets);
 
 /*
 #if useShuffleRule
@@ -151,15 +179,15 @@ int main() {
 //		strcat(tmpFileName, ruleFileName[q]);
 //		printProtocolType(&datasets, tmpFileName);
 
-#if useShuffleHeader == 0
-		strcpy(tmpFileName, "classbench_256k_s/");
-#elif useShuffleHeader == 1
-		strcpy(tmpFileName, "classbench_256k/");
-#else
-		strcpy(tmpFileName, "acl_128k_test/");
-#endif
-		strcat(tmpFileName, headFileName[q]);
-		read_messages(tmpFileName, &message_sets);
+//#if useShuffleHeader == 0
+//		strcpy(tmpFileName, "classbench_256k_s/");
+//#elif useShuffleHeader == 1
+//		strcpy(tmpFileName, "classbench_256k/");
+//#else
+//		strcpy(tmpFileName, "acl_128k_test/");
+//#endif
+//		strcat(tmpFileName, headFileName[q]);
+		read_messages(headFileName[q], &message_sets);
 
 //		int numUnit = (datasets.size + sizeof(char) - 1) / sizeof(char);
 //		unsigned char ** bitset=(unsigned char **) malloc(256 * sizeof(unsigned char*));
@@ -167,7 +195,7 @@ int main() {
 //			bitset[i]=(unsigned char *) calloc(numUnit, sizeof(unsigned char));
 //		}
 
-		unsigned long **bitset;
+		unsigned long** bitset;
 		init_bitset_IPv4(datasets.size, &bitset);
 
 		double insertCycle = GetCPUCycle();
@@ -175,11 +203,11 @@ int main() {
 //			insert(index, datasets.list + i);
 //			insert_cache(index, datasets.list + i,bitset);
 			insert_bitset_forward_IPv4(datasets.list + i, bitset);
-		insertCycle = ((double) GetCPUCycle() - insertCycle) / datasets.size;
+		insertCycle = ((double)GetCPUCycle() - insertCycle) / datasets.size;
 		totalInsertCycle += insertCycle;
 		printf("%s\navgInsertCycle= %f\n%d %d %d n_rule=%d, n_head=%d\n", ruleFileName[q], insertCycle, sizeof(data),
-			   sizeof(rule), sizeof(Cell),
-			   datasets.size, message_sets.size);
+			sizeof(rule), sizeof(Cell),
+			datasets.size, message_sets.size);
 //		printf("MemoryUse: %f MB\n", get_memory(index));
 //		get_cell_size(index);
 
@@ -195,8 +223,8 @@ int main() {
 		int oneCycle = 0;
 		int cycleList[message_sets.size];
 		MatchLog match_log;
-		match_log.list = (LogInCell *) malloc(16 * sizeof(LogInCell));
-		FILE *res_fp = NULL;
+		match_log.list = (LogInCell*)malloc(16 * sizeof(LogInCell));
+		FILE* res_fp = NULL;
 		strcpy(tmpFileName, "output/match_cycle_");
 		strcat(tmpFileName, ruleFileName[q]);
 		res_fp = fopen(tmpFileName, "w");
@@ -205,16 +233,21 @@ int main() {
 		uint64_t time_1, time_2;
 		time_1 = GetCPUCycle();
 //		clock_t start = clock();
-		for (int i = 0; i < message_sets.size; i += intervalRule) {
+		for (int i = 0; i < message_sets.size; i += intervalRule)
+		{
 //			res = match_with_log(index, message_sets.list + i, &oneCycle, &match_log);
 //			res = match_with_log_cache(index, message_sets.list + i, bitset, &oneCycle, &match_log);
 			res = match_bitset_forward_IPv4(datasets.list - 1, message_sets.list + i, bitset, &oneCycle, &checkNum);
-			if (res != (message_sets.list + i)->rule_id) {
-				if (res == -1) {
+			if (res != (message_sets.list + i)->rule_id)
+			{
+				if (res == -1)
+				{
 					if ((message_sets.list + i)->rule_id != 0)
 						printf("%d fails: %d !=%d.\n", i, res, (message_sets.list + i)->rule_id);
 					assert((message_sets.list + i)->rule_id == 0);
-				} else {
+				}
+				else
+				{
 					printf("%d header fails: %d !=%d.\n", i, res, (message_sets.list + i)->rule_id);
 					assert(0);
 				}
@@ -234,12 +267,12 @@ int main() {
 #endif
 		}
 		time_2 = GetCPUCycle();
-		setCycle = (double) (time_2 - time_1);
+		setCycle = (double)(time_2 - time_1);
 		totalMatchCycle += setCycle / message_sets.size;
 		printf("avgCycle= %f\n\n", setCycle / message_sets.size);
 		gettimeofday(&endtime, 0);
 		double timeuse = 1000000 * (endtime.tv_sec - starttime.tv_sec) + endtime.tv_usec - starttime.tv_usec;
-		printf("avgCheckNum= %.2f\n", (double) checkNum / message_sets.size);
+		printf("avgCheckNum= %.2f\n", (double)checkNum / message_sets.size);
 		printf("avgMatchTime= %fus\n\n", timeuse / message_sets.size);
 //		clock_t stop = clock();
 //		double elapsed = (double)(stop - start) * 1000.0 / CLOCKS_PER_SEC;
